fb22580d71b21aff32dd16de03b2d37d6076c1a5
davidddsdutra-byte <david.dds.dutra@gmail.com>
2026-03-19 23:00:06 +0000

    #include <stdlib.h> #include <time.h>

--- Desenvolvendo a Lógica.c
+++ Desenvolvendo a Lógica.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
